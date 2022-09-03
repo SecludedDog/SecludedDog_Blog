6aaa1203d6ddbd715d10152c780d35a89916a1db
SecludedDog <xiadayu2022@gmail.com>
2022-09-03 15:23:38 +0800

    what is undefined

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -21,7 +21,7 @@ export default function SinglePost({ post }) {
   //       setPosts(newPosts)
   //     })
   //   }, [])
-  console.log(`this is post details ${post}`)
+  console.log(`hello this is post details ${post}`)
 
   useEffect(() => {
     Aos.init({ duration: 800 })
